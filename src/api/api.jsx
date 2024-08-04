76dd4e792e830d9324a4a77538f0987a2f83169c
Shekhar <shekharsuman689@gmail.com>
2024-08-04 16:44:04 +0530

    change

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export async function productsData(){
     const products = await axios.get(
           
-         "https://meesho-backend-eight.vercel.app/api"
+         "https://meesho-backend-eight.vercel.app/api/product/details"
     );
         
     return products
